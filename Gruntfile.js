f0cc6576a5f5bc6df0072f7ceaa6307a768734c0
Angel <angel1.lopez@gmail.com>
2013-04-25 18:14:28 +0200

    anadidas opciones de validacion globales e individucalizacion de las validaciones de los modulos+

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,11 +15,53 @@ module.exports = function (grunt) {
 			}
 		},
 		jshint: {
-			all:{
+			options:{
+				//Neutral enviroment
+				//funciones de jshint ver descripcion en su sitio
+				browser : false,
+				node : false,
+				jquery: false,
+				strict : true,
+				globalstrict : true,
+				bitwise : true,
+				camelcase : true,
+				curly : true,
+				eqeqeq : true,
+				forin : true,
+				latedef :true,
+				newcap : true,
+				noarg : true,
+				nonew : true,
+				undef : true,
+				unused : true,
+				white : true,
+				trailing : true,
+				maxparams :3,
+				maxstatements : 10,
+				maxdepth : 2,
+				maxcomplexity : 5
+			},
+			core : {				
 				files : {
-					src : [SCRIPTS_DIR,'Gruntfile.js']
+					src : [SCRIPTS_DIR+'core/**/*.js']
 				}
-			}	
+			},
+			jquery : {
+				files : {
+					src : [SCRIPTS_DIR+'zepto_jquery/**/*.js']
+				}
+			},
+			knockout : {
+				files : {
+					src : [SCRIPTS_DIR+'knockout/**/*.js']
+				}
+			},
+			gruntfile : {
+				files : {
+					src : ['Gruntfile.js']
+				}
+			}
+			
 		}
 
 				
