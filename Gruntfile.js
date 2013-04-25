c75fa375fa75095e0ee8657e4b170b541d42d138
Angel <angel1.lopez@gmail.com>
2013-04-25 17:55:28 +0200

    anadido jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,12 +8,21 @@ module.exports = function (grunt) {
 					src : ['src/css/**/*.css']
 				}
 			}
+		},
+		jshint: {
+			all:{
+				files : {
+					src : ['src/lib/**/*.js','Gruntfile.js']
+				}
+			}	
 		}
+
 				
 	});
 	grunt.loadNpmTasks("grunt-contrib-csslint");
+	grunt.loadNpmTasks("grunt-contrib-jshint");
 
 	grunt.registerTask('default', [
-		'csslint'
+		'csslint','jshint'
 	]);
 };
